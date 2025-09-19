395fe6111ada823ab03d4ac51de87d8c632b7797
TejasShirsath <tejasshirsath5@gmail.com>
2025-09-19 16:10:24 +0530

    initialize Prisma ORM with MySQL setup

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require ('express');
+const {PrismaClient} = require('@prisma/client');
+
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+
+const prisma = global.prisma || new PrismaClient();
+if(process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 app.get('/',(req, res)=>{
     res.json({"msg": "Hello World!"})
 })
 
-app.listen(port, ()=>{
+app.get('/user', async(req, res) => {
+    try{
+        const users = await prisma.User.findMany();
+        res.json(users);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ error: 'Faild to fetch users'});
+    }
+});
+
+const server = app.listen(port, ()=>{
     console.log(`App listening on port ${port}`)
 })
+
+// graceful shutdown
+const shutdown = async() => {
+    console.log('shutting down server...');
+    server.close(async ()=>{
+        await prisma.$disconnect();
+        process.exit(0);
+    });
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGABRT', shutdown);
